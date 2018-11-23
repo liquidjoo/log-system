a9bfbe4e4e952bed4893fa60b03c7561bf5d24f0
liquidjoo <liquidjoo@gmail.com>
2018-11-23 17:17:10 +0900

    spring init

---
+++ src/main/java/com/liquidjoo/logsystem/LogsystemApplication.java
@@ -0,0 +1,12 @@
+package com.liquidjoo.logsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsystemApplication.class, args);
+    }
+}
